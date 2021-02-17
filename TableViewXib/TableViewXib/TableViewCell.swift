056b2742d02e49cc644b978a8126c21933e8b2ad
geethasreepada <59782179+geethasreepada@users.noreply.github.com>
2021-02-16 21:38:55 -0800

    Add files via upload

---
+++ TableViewXib/TableViewXib/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TableViewXib
+//
+//  Created by alkadios on 2/16/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblcities: UILabel!
+    
+    @IBOutlet weak var lblTemperatures: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+}
