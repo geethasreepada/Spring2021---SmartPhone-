a2f6e7bbc4b89c710cb850452c9c379f7b55abaf
geethasreepada <59782179+geethasreepada@users.noreply.github.com>
2021-02-20 15:31:32 -0800

    Add files via upload

---
+++ FirstAndLastname/FirstAndLastname/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  FirstAndLastname
+//
+//  Created by alkadios on 2/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var lblPressMe: UILabel!
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    
+   
+    var firstNameBtn : Bool = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func PressMe(_ sender: Any) {
+        
+        
+        
+        lblPressMe.text = "Button Pressed"
+        
+        if firstNameBtn == true{
+            firstNameBtn = false
+            imgView.image = UIImage(named:"SREEPADA")
+            
+        }
+        else{
+            firstNameBtn = true
+            imgView.image = UIImage(named:"GEETHA")
+        }
+    }
+    }
+    
+   
+
+
+
+
+
+
+
+
+//
+
